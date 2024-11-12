072f6656b691fa35cb4c4c1f4466dd65d9dcfadc
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-11-12 21:24:01 +0300

    feat(free-foil-stlc): - add simple functionality for working with stlc - add simple demo in Main

--- free-foil-stlc/app/Main.hs
+++ free-foil-stlc/app/Main.hs
@@ -1,5 +1,13 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib (hello)
+import Language.STLC.Common ()
+import Language.STLC.Syntax.Abs (Command)
+import Language.STLC.Syntax.Print (printTree)
+import Main.Utf8 (withUtf8)
 
-main = hello
+main :: IO ()
+main = withUtf8 do
+    putStrLn $ printTree ("#typecheck x:unit |- \\x.(\\z.z) x <= unit" :: Command)
---
+++ free-foil-stlc/src/Language/STLC/Common.hs
@@ -0,0 +1,36 @@
+module Language.STLC.Common where
+
+import Data.String (IsString (..))
+import Language.STLC.Syntax.Abs
+import Language.STLC.Syntax.Lex (Token)
+import Language.STLC.Syntax.Par
+import Language.STLC.Syntax.Print
+
+-- $setup
+-- >>> :set -XOverloadedStrings
+
+instance IsString Exp where fromString = unsafeParseWith pExp
+instance IsString Type where fromString = unsafeParseWith pType
+instance IsString Ctx where fromString = unsafeParseWith pCtx
+instance IsString ExpUnderCtx where fromString = unsafeParseWith pExpUnderCtx
+instance IsString Command where fromString = unsafeParseWith pCommand
+
+parseWith :: ([Token] -> Either String a) -> String -> Either String a
+parseWith parser input = parser tokens
+ where
+  tokens = myLexer input
+
+-- | Parse from a 'String'.
+-- May throw an 'error` if input has syntactical or lexical errors.
+unsafeParseWith :: ([Token] -> Either String a) -> String -> a
+unsafeParseWith parser input =
+  case parseWith parser input of
+    Left parseError -> error (parseError <> "\non input\n" <> input <> "\n")
+    Right res -> res
+
+-- >>> "#typecheck x : unit |- \\ x . (\\ z . z) x <= unit -> unit" :: Command
+-- CommandTypeCheck (ExpUnderCtx (Ctx [CtxVar (Var "x") TypeUnit]) (ExpAbs (Var "x") (ExpApp (ExpAbs (Var "z") (ExpVar (Var "z"))) (ExpVar (Var "x"))))) (TypeFunc TypeUnit TypeUnit)
+
+-- >>> printTree $ ("#typecheck x : unit |- \\x.(\\z.z) x <= unit -> unit" :: Command)
+-- "#typecheck x : unit |- \\ x . (\\ z . z) x <= unit -> unit"
+
