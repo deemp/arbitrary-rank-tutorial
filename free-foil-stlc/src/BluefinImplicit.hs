da41dd6ec2c9b29a0cf91b0ee15fabf4d2d33975
Danila Danko <br4ch1st0chr0n3@gmail.com>
2025-05-13 18:03:22 +0300

    fix(free-foil-stlc): improve bluefin interface

--- free-foil-stlc/src/BluefinImplicit.hs
+++ free-foil-stlc/src/BluefinImplicit.hs
@@ -1,16 +1,22 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module BluefinImplicit where
 
 import Bluefin.Internal
 import Control.Monad (forever, when)
+import Data.Kind (Constraint)
 import GHC.Base (IP)
 import Prelude hiding (break)
 
@@ -18,32 +24,42 @@ import Prelude hiding (break)
 -- Library code
 -- ~~~~~~~~~~~~
 
+type family (::>) (es1 :: [Effects]) (es2 :: Effects) :: Constraint where
+  (x : xs) ::> ys = (x :> ys, xs ::> ys)
+  '[] ::> ys = ()
+
 -- State
 
+type StateI h s e es = (IP h (State s e), e :> es)
+
 -- evalState :: forall s (es :: Effects) a. s -> (forall (e :: Effects). State s e -> Eff (e :& es) a) -> Eff es a
-type EvalStateI h = forall s (es :: Effects) a. s -> (forall (e :: Effects). (e :> (e :& es)) => (IP h (State s e)) => Eff (e :& es) a) -> Eff es a
+type EvalStateI h = forall s (es :: Effects) a. s -> (forall (e :: Effects). (StateI h s e (e :& es)) => Eff (e :& es) a) -> Eff es a
 
 -- get :: forall (e :: Effects) (es :: Effects) s. (e :> es) => State s e -> Eff es s
-type GetI h = forall (e :: Effects) (es :: Effects) s. (e :> es) => (IP h (State s e)) => Eff es s
+type GetI h = forall (e :: Effects) (es :: Effects) s. (e :> es) => (StateI h s e es) => Eff es s
 
 -- modify :: forall (e :: Effects) (es :: Effects) s. (e :> es) => State s e -> (s -> s) -> Eff es ()
-type ModifyI h = forall (e :: Effects) (es :: Effects) s. (e :> es) => (IP h (State s e)) => (s -> s) -> Eff es ()
+type ModifyI h = forall (e :: Effects) (es :: Effects) s. (e :> es) => (StateI h s e es) => (s -> s) -> Eff es ()
 
 -- Reader
 
+type ReaderI h r e es = (IP h (Reader r e), e :> es)
+
 -- runReader :: forall r (es :: Effects) a. r -> (forall (e :: Effects). Reader r e -> Eff (e :& es) a) -> Eff es a
-type RunReaderI h = forall r (es :: Effects) a. r -> (forall (e :: Effects). (e :> (e :& es)) => (IP h (Reader r e)) => Eff (e :& es) a) -> Eff es a
+type RunReaderI h = forall r (es :: Effects) a. r -> (forall (e :: Effects). (ReaderI h r e (e :& es)) => Eff (e :& es) a) -> Eff es a
 
 -- ask :: forall (e :: Effects) (es :: Effects) r. (e :> es) => Reader r e -> Eff es r
-type AskI h = forall (e :: Effects) (es :: Effects) r. (e :> es) => (IP h (Reader r e)) => Eff es r
+type AskI h = forall (e :: Effects) (es :: Effects) r. (e :> es) => (ReaderI h r e es) => Eff es r
 
 -- IO
 
+type IOEI h e es = (IP h (IOE e), e :> es)
+
 -- runEff :: forall a. (forall (e :: Effects) (es :: Effects). IOE e -> Eff (e :& es) a) -> IO a
-type RunEffI h = forall r. (forall (e :: Effects) (es :: Effects). (e :> (e :& es)) => (IP h (IOE e)) => Eff (e :& es) r) -> IO r
+type RunEffI h = forall r. (forall (e :: Effects) (es :: Effects). (IOEI h e (e :& es)) => Eff (e :& es) r) -> IO r
 
 -- effIO :: forall (e :: Effects) (es :: Effects) a. (e :> es) => IOE e -> IO a -> Eff es a
-type EffIOI h = forall (e :: Effects) (es :: Effects) a. (e :> es) => (IP h (IOE e)) => IO a -> Eff es a
+type EffIOI h = forall (e :: Effects) (es :: Effects) a. (e :> es) => (IOEI h e es) => IO a -> Eff es a
 
 -- ~~~~~~~~~
 -- User code
@@ -94,10 +110,20 @@ countExampleI =
       r <- askI
       evalStateI1 @Double 0 do
         evalStateI @Int r do
-          withJump $ \break -> forever do
-            int' <- getI
-            when (int' >= 10) (jumpTo break)
-            double' <- getI1
-            effIOI (print ((fromIntegral int') + double'))
-            modifyI (+ 1)
-            modifyI1 (+ 0.1)
+          withJump $ \break ->
+            forever (actionI1 break)
+
+actionI1 ::
+  ( IOEI "io" e1 es
+  , StateI "st" Int e2 es
+  , StateI "st1" Double e3 es
+  , e4 :> es
+  ) =>
+  Jump e4 -> Eff es ()
+actionI1 break = do
+  int' <- getI
+  when (int' >= 10) (jumpTo break)
+  double' <- getI1
+  effIOI (print ((fromIntegral int') + double'))
+  modifyI (+ 1)
+  modifyI1 (+ 0.1)
