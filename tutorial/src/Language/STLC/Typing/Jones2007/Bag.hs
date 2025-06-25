4ae916391f8648c0ad608c93a4abf6020b492067
Danila Danko <br4ch1st0chr0n3@gmail.com>
2025-06-25 19:22:44 +0300

    feat(tutorial): add bag

---
+++ tutorial/src/Language/STLC/Typing/Jones2007/Bag.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Language.STLC.Typing.Jones2007.Bag where
+
+-- GHC uses a tree-like structure for the `Bag`.
+-- A list will be enough in our case.
+-- https://github.com/ghc/ghc/blob/ed38c09bd89307a7d3f219e1965a0d9743d0ca73/compiler/GHC/Data/Bag.hs#L48
+newtype Bag a = Bag {bag :: [a]}
+  deriving newtype (Functor, Semigroup)
+
+unitBag :: a -> Bag a
+unitBag x = Bag [x]
+
+unionBags :: Bag a -> Bag a -> Bag a
+unionBags b1 b2 = b1 <> b2
+
+consBag :: a -> Bag a -> Bag a
+consBag el b = unitBag el `unionBags` b
