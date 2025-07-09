7e97f50b110ef00f6edfcf328342df026cb0c56b
Danila Danko <br4ch1st0chr0n3@gmail.com>
2025-07-09 12:00:08 +0300

    feat(tutorial): implement a simple free-foil-based interpreter that evaluates WHNF of a Core expression

---
+++ tutorial/src/Language/STLC/Interpreter/FreeFoil.hs
@@ -0,0 +1,118 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Language.STLC.Interpreter.FreeFoil where
+
+import Control.Monad.Foil (CoSinkable (..), DistinctEvidence (..), ExtEvidence (..))
+import Control.Monad.Foil.Internal (DExt, Name (..), NameBinder (..), Scope (..), SinkableK, Substitution (..), unsafeDistinct, unsafeExt)
+import Data.IntMap qualified as IntMap
+import Data.IntSet qualified as IntSet
+import Generics.Kind.TH
+import Language.STLC.Typing.Jones2007.BasicTypes qualified as BT
+import Prettyprinter
+import Unsafe.Coerce (unsafeCoerce)
+
+-- | A wrapper around 'NameBinder' that stores additional information.
+data CoreNameBinder n l
+  = CoreNameBinder
+  { name :: BT.Name
+  , nameBinder :: (NameBinder n l)
+  }
+
+deriveGenericK 'CoreNameBinder
+
+-- ** Extending scopes
+
+-- | \(O(\min(n,W))\).
+-- Extend a scope with one name (safely).
+-- Note that as long as the foil is used as intended,
+-- the name binder is guaranteed to introduce a name
+-- that does not appear in the initial scope.
+extendScope :: CoreNameBinder n l -> Scope n -> Scope l
+extendScope CoreNameBinder{nameBinder = UnsafeNameBinder (UnsafeName name)} (UnsafeScope scope) =
+  UnsafeScope (IntSet.insert name scope)
+
+-- ** Refreshing binders
+
+-- TODO check scope
+
+-- | Allocate a fresh binder for a given scope.
+withFreshBinderUsingUnique ::
+  Scope n ->
+  BT.Name ->
+  (forall l. CoreNameBinder n l -> r) ->
+  r
+withFreshBinderUsingUnique (UnsafeScope _scope) name cont =
+  cont binder
+ where
+  binder =
+    CoreNameBinder
+      { name
+      , nameBinder = UnsafeNameBinder (UnsafeName (name.nameUnique))
+      }
+
+-- | Unsafely declare that a given name (binder)
+-- is already fresh in any scope @n'@.
+unsafeAssertFresh ::
+  forall n l n' l' r.
+  CoreNameBinder n l ->
+  ((DExt n' l') => CoreNameBinder n' l' -> r) ->
+  r
+unsafeAssertFresh binder cont =
+  case unsafeDistinct @l' of
+    Distinct -> case unsafeExt @n' @l' of
+      Ext -> cont (unsafeCoerce binder)
+
+-- | Safely produce a fresh name binder with respect to a given scope.
+withFreshUsingUnique ::
+  Scope n ->
+  BT.Name ->
+  (forall l. (DExt n l) => CoreNameBinder n l -> r) ->
+  r
+withFreshUsingUnique scope name cont = withFreshBinderUsingUnique scope name (`unsafeAssertFresh` cont)
+
+-- * Safe substitions
+
+-- | Extend substitution with a particular mapping.
+addSubst ::
+  Substitution e i o ->
+  CoreNameBinder i i' ->
+  e o ->
+  Substitution e i' o
+addSubst
+  (UnsafeSubstitution env)
+  (CoreNameBinder{nameBinder = UnsafeNameBinder (UnsafeName name')})
+  ex =
+    UnsafeSubstitution (IntMap.insert name' ex env)
+
+instance CoSinkable CoreNameBinder where
+  coSinkabilityProof _rename CoreNameBinder{name, nameBinder = UnsafeNameBinder name'} cont =
+    cont unsafeCoerce CoreNameBinder{name, nameBinder = UnsafeNameBinder name'}
+
+  withPattern f _ _ scope coreNameBinder coercion =
+    (f scope coreNameBinder.nameBinder)
+      (\x y -> coercion x CoreNameBinder{name = error "Impossible!", nameBinder = y})
+
+instance SinkableK CoreNameBinder
+
+newtype PrettyName n = PrettyName (Name n)
+
+newtype PrettyNameBinder n l = PrettyNameBinder (NameBinder n l)
+
+instance Pretty (PrettyName n) where
+  pretty (PrettyName (UnsafeName n)) = "x_" <> pretty n
+
+instance Pretty (PrettyNameBinder n l) where
+  pretty (PrettyNameBinder (UnsafeNameBinder name)) = pretty (PrettyName name)
+
+instance Pretty (CoreNameBinder n l) where
+  pretty binder = pretty (PrettyNameBinder binder.nameBinder)
+
---
+++ tutorial/src/Language/STLC/Interpreter/Main.hs
@@ -0,0 +1,102 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+module Language.STLC.Interpreter.Main where
+
+import Control.Monad.Foil.Internal (DExt, Distinct, Name (..), Scope (..), identitySubst)
+import Control.Monad.Free.Foil (AST (..), ScopedAST (..), substitute)
+import Data.Bifunctor.TH (deriveBifunctor)
+import Language.STLC.Interpreter.FreeFoil (CoreNameBinder (..), PrettyName (..), addSubst, extendScope, withFreshUsingUnique)
+import Language.STLC.Typing.Jones2007.BasicTypes (CompZn, SynLit, SynTerm (..))
+import Language.STLC.Typing.Jones2007.BasicTypes qualified as BT
+import Prettyprinter
+
+-- TODO Delayed substitution and Normalization by Evaluation
+
+data Core scope term
+  = Core'Lit SynLit
+  | Core'App term term
+  | Core'Lam scope
+  | Core'Let term scope
+  deriving stock (Functor)
+
+deriveBifunctor ''Core
+
+pattern LitE :: SynLit -> AST binder Core n
+pattern LitE x = Node (Core'Lit x)
+
+pattern AppE :: AST binder Core n -> AST binder Core n -> AST binder Core n
+pattern AppE x y = Node (Core'App x y)
+
+pattern LamE :: binder n l -> AST binder Core l -> AST binder Core n
+pattern LamE binder body = Node (Core'Lam (ScopedAST binder body))
+
+pattern LetE :: binder n l -> AST binder Core n -> AST binder Core l -> AST binder Core n
+pattern LetE binder body rhs = Node (Core'Let body (ScopedAST binder rhs))
+
+{-# COMPLETE LitE, AppE, LamE, LetE #-}
+
+type CoreE = AST CoreNameBinder Core
+
+litE :: SynLit -> AST binder Core n
+litE = LitE
+
+appE :: CoreE n -> CoreE n -> CoreE n
+appE = AppE
+
+lamE :: Scope n -> BT.Name -> (forall l. (DExt n l) => Scope l -> CoreE l) -> CoreE n
+lamE scope name' mkBody = withFreshUsingUnique scope name' $ \x ->
+  let scope' = extendScope x scope
+   in LamE x (mkBody scope')
+
+letE :: Scope n -> BT.Name -> CoreE n -> (forall l. (DExt n l) => Scope l -> CoreE l) -> CoreE n
+letE scope name' body mkRhs = withFreshUsingUnique scope name' $ \x ->
+  let scope' = extendScope x scope
+   in LetE x body (mkRhs scope')
+
+convertASTToCore :: Scope n -> SynTerm CompZn -> CoreE n
+convertASTToCore scope = \case
+  SynTerm'Lit _ lit ->
+    Node (Core'Lit lit)
+  SynTerm'App _ fun arg ->
+    appE (convertASTToCore scope fun) (convertASTToCore scope arg)
+  SynTerm'Lam _ var body ->
+    lamE scope var.varName (\scope' -> convertASTToCore scope' body)
+  SynTerm'ALam _ var _ body ->
+    lamE scope var.varName (\scope' -> convertASTToCore scope' body)
+  SynTerm'Let _ var body rhs ->
+    letE scope var.varName (convertASTToCore scope body) (\scope' -> convertASTToCore scope' rhs)
+  SynTerm'Ann _ term _ ->
+    convertASTToCore scope term
+  SynTerm'Var _ var -> Var (UnsafeName var.varName.nameUnique)
+
+whnf :: (Distinct n) => Scope n -> CoreE n -> CoreE n
+whnf scope = \case
+  AppE fun arg ->
+    case whnf scope fun of
+      LamE binder body ->
+        let subst = addSubst identitySubst binder arg
+         in whnf scope (substitute scope subst body)
+      fun' -> AppE fun' arg
+  LetE binder body rhs ->
+    let subst = addSubst identitySubst binder body
+     in whnf scope (substitute scope subst rhs)
+  t -> t
+
+instance Pretty (CoreE n) where
+  pretty = \case
+    Node x -> case x of
+      Core'Lit lit -> pretty lit
+      Core'App fun arg -> parens (pretty fun) <+> parens (pretty arg)
+      Core'Lam (ScopedAST binder body) -> "\\" <> pretty binder <> "." <+> pretty body
+      Core'Let body (ScopedAST binder rhs) -> "let" <+> pretty binder <+> "=" <+> pretty body <+> "in" <+> pretty rhs
+    Var x -> pretty (PrettyName x)
