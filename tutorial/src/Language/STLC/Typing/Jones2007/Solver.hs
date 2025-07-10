47a4565767bb856a858ebb16799e810674987b8c
Danila Danko <br4ch1st0chr0n3@gmail.com>
2025-07-10 20:53:44 +0300

    fix(tutorial): improve solver - add unification - implement exceptions and their pretty-printing - reorder code - simplify types in "solve"

--- tutorial/src/Language/STLC/Typing/Jones2007/Solver.hs
+++ tutorial/src/Language/STLC/Typing/Jones2007/Solver.hs
@@ -1,13 +1,85 @@
+{-# LANGUAGE OverloadedLabels #-}
+
 module Language.STLC.Typing.Jones2007.Solver where
 
+import Control.Exception (Exception, throw)
 import Control.Monad (forM, forM_, when)
 import Data.Foldable (Foldable (..))
-import Data.IORef (readIORef, writeIORef)
-import GHC.IORef (newIORef)
+import GHC.Exception (prettyCallStack)
+import GHC.Stack (HasCallStack, callStack)
 import Language.STLC.Typing.Jones2007.Bag
 import Language.STLC.Typing.Jones2007.BasicTypes
-import Language.STLC.Typing.Jones2007.ConstraintTypes
-import Language.STLC.Typing.Jones2007.TcMonad (TcError (..), TcM, die, readTcRef, unify, writeTcRef)
+import Language.STLC.Typing.Jones2007.Constraints
+import Language.STLC.Typing.Jones2007.TcMonad (IConstraints, ITcErrorPropagated, TcError (..), TcM, badType, debug, die, readTcRef, writeTcRef)
+import Prettyprinter (line)
+
+data SolverError
+  = SolverError'CannotUnifySkolemVar {ct :: Ct}
+  | SolverError'CannotUnifyBoundVar {tv :: TcTyVar, ty :: TcType, ct :: Ct}
+  | SolverError'CannotUnify {ty1 :: TcType, ty2 :: TcType, ct :: Ct}
+  | SolverError'SkolemEscape {tv :: TcTyVar, ty :: TcType, tvTcLevel :: TcLevel, tyTcLevel :: TcLevel, ct :: Ct}
+
+data SolverErrorWithCallStack where
+  SolverErrorWithCallStack :: (HasCallStack) => SolverError -> SolverErrorWithCallStack
+
+-- | Fail unconditionally
+dieSolver :: (HasCallStack) => SolverError -> IO a
+dieSolver err = throw (SolverErrorWithCallStack err)
+
+type SolveM a b = a -> TcM b
+
+toListWc :: WantedConstraints -> [WantedConstraints]
+toListWc = \case
+  WantedCts{wc_simple = Bag [], wc_impl = Bag []} -> []
+  x -> [x]
+
+fromListWc :: [WantedConstraints] -> WantedConstraints
+fromListWc = fold
+
+unify :: (ITcErrorPropagated, IConstraints) => Ct -> Tau -> Tau -> IO [Ct]
+-- Invariant:
+-- The first type is "expected",
+-- the second one is "actual"
+unify ct tv'@(Type'Var tv) ty
+  | badType tv' =
+      dieSolver SolverError'CannotUnifyBoundVar{tv, ty, ct}
+unify ct ty tv'@(Type'Var tv)
+  | badType tv' =
+      dieSolver SolverError'CannotUnifyBoundVar{tv, ty, ct}
+unify
+  _ct
+  (Type'Var TcTyVar{varDetails = MetaTv{metaTvRef = tv1}})
+  (Type'Var TcTyVar{varDetails = MetaTv{metaTvRef = tv2}})
+    | tv1 == tv2 = pure []
+-- TODO Pass info about swapping?
+unify ct (Type'Var tv@TcTyVar{varDetails = MetaTv{}}) ty =
+  pure $ unifyVar ct tv ty
+unify ct ty (Type'Var tv@TcTyVar{varDetails = MetaTv{}}) =
+  pure $ unifyVar ct tv ty
+-- TODO is equality defined correctly?
+unify _ct (Type'Var tv1@TcTyVar{}) (Type'Var tv2@TcTyVar{})
+  | tv1 == tv2 = pure []
+unify ct (Type'Fun arg1 res1) (Type'Fun arg2 res2) = do
+  -- TODO should we inspect thing?
+  cts1 <- unify ct arg1 arg2
+  cts2 <- unify ct res1 res2
+  pure $ cts1 <> cts2
+unify _ct (Type'Concrete tc1) (Type'Concrete tc2)
+  | tc1 == tc2 = pure []
+unify ct ty1 ty2 =
+  dieSolver SolverError'CannotUnify{ty1, ty2, ct}
+
+unifyVar :: Ct -> TcTyVar -> Tau -> [Ct]
+unifyVar ct tv@TcTyVar{varDetails = MetaTv{}} ty = do
+  pure $
+    ct
+      { ct_eq_can =
+          ct.ct_eq_can
+            { eq_lhs = tv
+            , eq_rhs = ty
+            }
+      }
+unifyVar _ct _tv _ty = []
 
 class MaxTcLevel a where
   getMaxTcLevel :: a -> TcLevel
@@ -18,59 +90,61 @@ instance MaxTcLevel TcTyVar where
 instance MaxTcLevel TcType where
   getMaxTcLevel = \case
     Type'Var var -> var.varDetails.tcLevel
-    -- 'maximum' is safe to use because
-    -- the structure isn't empty
+    -- 'maximum' is safe to use because the structure isn't empty
     Type'ForAll vars body -> maximum ((getMaxTcLevel <$> vars) <> [getMaxTcLevel body])
     Type'Fun arg res -> maximum ([getMaxTcLevel arg, getMaxTcLevel res])
     Type'Concrete _ -> 0
-
-type SolveM a b = a -> TcM b
 
 class Solve a where
   type Ret a
   solve :: SolveM a (Ret a)
   occursCheck :: SolveM a ()
 
-toListWc :: WantedConstraints -> [WantedConstraints]
-toListWc = \case
-  WantedCts{wc_simple = Bag [], wc_impl = Bag []} -> []
-  x -> [x]
-
-fromListWc :: [WantedConstraints] -> WantedConstraints
-fromListWc = fold
-
 instance Solve Ct where
-  type Ret Ct = [WantedConstraints]
+  type Ret Ct = [Ct]
   solve ct = do
     let lhs = ct.ct_eq_can.eq_lhs
         rhs = ct.ct_eq_can.eq_rhs
     case lhs.varDetails of
-      SkolemTv{} -> die TcError'CannotUnifySkolemVar{ct}
-      BoundTv{} -> die TcError'CannotUnifyBoundVar{ct}
+      -- TODO why can we get not a metavar on LHS?
+      SkolemTv{} -> dieSolver SolverError'CannotUnifySkolemVar{ct}
+      BoundTv{} -> dieSolver SolverError'CannotUnifyBoundVar{tv = lhs, ty = rhs, ct}
       MetaTv{metaTvRef} -> do
         let lhsLevel = getMaxTcLevel lhs
             rhsLevel = getMaxTcLevel rhs
-        if rhsLevel <= lhsLevel
-          then do
-            metaDetails <- readTcRef metaTvRef
-            case metaDetails of
-              Flexi -> do
-                writeTcRef metaTvRef (Indirect rhs)
-                pure []
-              Indirect ty -> do
-                writeTcRef ?constraints emptyWantedConstraints
-                -- TODO propagate Ct info into each new constraint?
-                unify Nothing ty rhs
-                -- Have to do this because unify doesn't return constraints
-                constraintsNew <- readTcRef ?constraints
-                writeTcRef ?constraints emptyWantedConstraints
-                pure $ toListWc constraintsNew
-          else die TcError'SkolemEscape{ct}
+        if
+          | lhsLevel >= rhsLevel -> do
+              metaDetails <- readTcRef metaTvRef
+              case metaDetails of
+                Flexi -> do
+                  debug
+                    "solve Ct - Flexi"
+                    [ "ct:" <> line <> prettyVerbose ct
+                    ]
+                  writeTcRef metaTvRef (Indirect rhs)
+                  pure []
+                Indirect ty -> do
+                  debug
+                    "solve Ct - Indirect"
+                    [ "ct:" <> line <> prettyVerbose ct
+                    ]
+                  constraintsNew <- unify ct ty rhs
+                  pure constraintsNew
+          | otherwise ->
+              dieSolver
+                SolverError'SkolemEscape
+                  { tv = lhs
+                  , ty = rhs
+                  , tvTcLevel = lhsLevel
+                  , tyTcLevel = rhsLevel
+                  , ct
+                  }
+
   occursCheck ct = do
     let lhs = ct.ct_eq_can.eq_lhs
         rhs = ct.ct_eq_can.eq_rhs
-        -- TODO recurse into Indirect type in Type'Var
-        -- Can this cause infinite recursion?
+        -- TODO Should we recurse into Indirect type in Type'Var?
+        -- This can cause infinite recursion.
         occursIn :: TcTyVar -> TcType -> Bool
         occursIn var = \case
           Type'Var var' -> var == var'
@@ -80,7 +154,7 @@ instance Solve Ct where
     when (lhs `occursIn` rhs) $ die TcError'OccursCheck{ct}
 
 instance Solve Cts where
-  type Ret Cts = [WantedConstraints]
+  type Ret Cts = [Ct]
   solve cts = concat <$> forM (reverse cts.bag) solve
   occursCheck cts = forM_ cts.bag occursCheck
 
@@ -97,27 +171,69 @@ instance Solve Impls where
     forM_ (reverse impls.bag) (\impl -> occursCheck impl.ic_wanted)
 
 instance Solve WantedConstraints where
+  -- We don't want 'WantedConstraints' with fields containing empty lists.
+  -- Therefore, we return a possibly empty list of folded 'WantedConstraints'.
   type Ret WantedConstraints = [WantedConstraints]
   solve wcs = do
-    wc_simple <- fold <$> solve wcs.wc_simple
-    wc_impl <- solve wcs.wc_impl
-    let wcs' = wc_simple <> WantedCts{wc_simple = emptyBag, wc_impl = Bag wc_impl}
+    wc_simple <- Bag <$> solve wcs.wc_simple
+    wc_impl <- Bag <$> solve wcs.wc_impl
+    let wcs' = WantedCts{wc_simple, wc_impl}
     pure $ toListWc wcs'
   occursCheck wcs = do
     occursCheck wcs.wc_simple
     occursCheck wcs.wc_impl
 
-solveIteratively :: TcM ()
-solveIteratively = do
-  forM_ ([0 .. 10] :: [Int]) $ \_ -> do
-    constraintsCurrent <- readIORef ?constraints
+instance Pretty' SolverError where
+  pretty' = \case
+    SolverError'CannotUnifySkolemVar{ct} ->
+      vsep'
+        [ "Cannot unify the skolem variable:"
+        , prettyIndent ct.ct_eq_can.eq_lhs
+        , "with the type:"
+        , prettyIndent ct.ct_eq_can.eq_rhs
+        , "in the constraint:"
+        , prettyIndent ct
+        ]
+    SolverError'CannotUnifyBoundVar{tv, ty, ct} ->
+      vsep'
+        [ "Cannot unify bound variable:"
+        , prettyIndent tv
+        , "with the type:"
+        , prettyIndent ty
+        , "in the constraint:"
+        , prettyIndent ct
+        ]
+    SolverError'CannotUnify{ty1, ty2, ct} ->
+      vsep'
+        [ "Cannot unify the type:"
+        , prettyIndent ty1
+        , "with the type:"
+        , prettyIndent ty2
+        , "in the constraint:"
+        , prettyIndent ct
+        ]
+    SolverError'SkolemEscape{tv, ty, tvTcLevel, tyTcLevel, ct} ->
+      vsep'
+        [ "Skolem escape!"
+        , "The variable:"
+        , prettyIndent tv
+        , "has the TcLevel:"
+        , prettyIndent tvTcLevel
+        , "but the type:"
+        , prettyIndent ty
+        , "has a larger TcLevel:"
+        , prettyIndent tyTcLevel
+        , "in the constraint:"
+        , prettyIndent ct
+        ]
 
-    -- Don't modify any external mutable variables.
-    constraintsNew <- newIORef emptyWantedConstraints
-    tcErrorPropagated <- newIORef Nothing
-    let ?constraints = constraintsNew
-        ?tcErrorPropagated = tcErrorPropagated
+instance Pretty' SolverErrorWithCallStack where
+  pretty' (SolverErrorWithCallStack err) =
+    vsep'
+      [ pretty' (prettyCallStack callStack)
+      , pretty' err
+      ]
 
-    wcs <- solve constraintsCurrent
+instance Exception SolverError
 
-    writeIORef ?constraints (fold wcs)
+instance Exception SolverErrorWithCallStack
