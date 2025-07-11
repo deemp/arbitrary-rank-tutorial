7b3e72e4281f99b6a9904a5342423f37ba76c97b
Danila Danko <br4ch1st0chr0n3@gmail.com>
2025-07-11 12:14:25 +0300

    fix(tutorial): update main example

--- tutorial/src/Language/STLC/Typing/Jones2007/Main.hs
+++ tutorial/src/Language/STLC/Typing/Jones2007/Main.hs
@@ -1,37 +1,32 @@
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE ImplicitParams #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE RankNTypes #-}
-
 module Language.STLC.Typing.Jones2007.Main where
-
--- import Data.Maybe
-
-import Prettyprinter
 
 import Control.Monad.Foil (emptyScope)
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Language.STLC.Interpreter.Main (convertASTToCore, whnf)
-import Language.STLC.Typing.Jones2007.BasicTypes (Pretty' (..), PrettyVerbosity (PrettyVerbosity'Compact))
-import Language.STLC.Typing.Jones2007.TcTerm
-import Prettyprinter.Render.Text
+import Language.STLC.Typing.Jones2007.BasicTypes (Pretty' (..), PrettyVerbosity (PrettyVerbosity'Normal), prettyUser)
+import Language.STLC.Typing.Jones2007.Solver (SolverErrorWithCallStack)
+import Language.STLC.Typing.Jones2007.TcMonad (TcErrorWithCallStack)
+import Language.STLC.Typing.Renamer (RnErrorWithCallStack)
+import Language.STLC.Typing.Run (runTypechecker')
+import Prettyprinter (line)
+import Prettyprinter.Render.Text (putDoc)
+import Prettyprinter.Util (putDocW)
+import UnliftIO (catch)
 import Prelude hiding (exp)
 
-t3 :: IO ()
-t3 = do
+main :: IO ()
+main = do
   let filePath = "test/data/Program1.stlc"
   content <- T.readFile filePath
   let ?debug = True
       ?prettyVerbosity = PrettyVerbosity'Normal
-  programZn <- runTypechecker' (T.pack filePath) content
-  putDoc $ line <> pretty' programZn <> line
-  putDoc $ line <> pretty' (whnf emptyScope (convertASTToCore emptyScope programZn)) <> line
-
--- >>> t3
--- "Exception!"
+      ?solverIterations = 10
+  programZn <- do
+    let prettyError x = putDocW 100 (pretty' x) >> error "Error!"
+    runTypechecker' (T.pack filePath) content
+      `catch` (\(x :: RnErrorWithCallStack) -> prettyError x)
+      `catch` (\(x :: TcErrorWithCallStack) -> prettyError x)
+      `catch` (\(x :: SolverErrorWithCallStack) -> prettyError x)
+  putDoc $ line <> prettyUser programZn <> line
+  putDoc $ line <> prettyUser (whnf emptyScope (convertASTToCore emptyScope programZn)) <> line
